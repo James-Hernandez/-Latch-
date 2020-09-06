c0579c86b2ec1636ced46de7efdeeae5d2d36bb2
Khai Pham <zekhai@mail.fresnostate.edu>
2020-09-05 20:29:47 -0700

    connect to mongo and add user schema

---
+++ _server_/configs/mongo.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const connecting_database = async () => {
+  try {
+    const database = config.get('mongo');
+    await mongoose.connect(
+      database, 
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+      }
+    );  
+  }
+  catch (err) {
+    console.err(err.message);
+    process.exit(1);
+  }
+}
+
+module.exports = connecting_database();
--- _server_/server.js
+++ _server_/server.js
@@ -5,5 +5,4 @@ const app = express();
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5000;
-
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
