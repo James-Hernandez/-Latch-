fd9b7d923069f01e8cdff5738c9167784871ee58
James Hernandez <happjnh@gmail.com>
2021-01-05 21:26:32 -0800

    added node-sass to packag.json

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TestMenu from './component/test-menu/test-menu.component';
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <TestMenu />
     </div>
   );
 }
