47ab075e7eaa2c0d77072ff37c10af54a9a2c315
Khai Pham <zekhai@mail.fresnostate.edu>
2021-01-06 21:30:55 -0800

    add register

--- client/src/features/register/scene.jsx
+++ client/src/features/register/scene.jsx
@@ -1,46 +1,72 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const NameForm = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
+const Register = () => {
   const [form, setForm] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: ""
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
   });
 
   useEffect(() => {
-    console.log("i wil rendre every sexonds");
+    console.log("I will render every seconds");
   })
 
-  const onKhai = (e,prop) => {
-    setForm({...form,[prop]:e.target.value});
-    console.log(form);
+  const onChange = (e,prop) => {
+    setForm({...form, [prop]:e.target.value});
   };
 
-
-  const handleSubmit = (e) => {
-    alert('A name was submitted: ' + form);
-    e.preventDefault();
+  const handleSubmit = (_) => {
+    const isSamePassword = form.password === form.confirmPassword;
+    if (isSamePassword) {
+      // go a function that create a new user and send it to mongoDB with axio
+      // then go to home rooute
+    }
   };
 
   return(
     <form onSubmit={handleSubmit}>
+      <div className='field-input'>
         <label>
-          Name:
-          <input type="text" value={form.firstName} onChange={(e) => onKhai(e,"firstName")} />
+          First Name:
+          <input type='text' value={form.firstName} onChange={(e) => onChange(e, 'firstName')} />
         </label>
-        <label>
-          Name:
-          <input type="text" value={form.lastName} onChange={(e) => onKhai(e,"lastName")} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      </div>
 
+      <div className='field-input'>
+        <label>
+          Last Name:
+          <input type='text' value={form.lastName} onChange={(e) => onChange(e, 'lastName')} />
+        </label>
+      </div>
+
+      <div className='field-input'>
+        <label>
+          Email:
+          <input type='email' value={form.email} onChange={(e) => onChange(e, 'email')} />
+        </label>
+      </div>
+
+      <div className='field-input'>
+        <label>
+          Password:
+          <input type='password' value={form.password} onChange={(e) => onChange(e, 'password')} />
+        </label>
+      </div>
+
+      <div className='field-input'>
+        <label>
+          Confirm Password:
+          <input type='password' value={form.confirmPassword} onChange={(e) => onChange(e, 'confirmPassword')} />
+        </label>
+      </div>
+      
+      <button type='submit' value='Submit'>
+        Button
+      </button>
+    </form>
   )
-
-
 }
-export default NameForm;
+
+export default Register;
